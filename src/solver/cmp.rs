6dd9f09079e605f5deb1367a7eb3831419a98506
Tymoteusz <twolodzko+git@gmail.com>
2024-09-17 09:27:30 +0200

    Remove dead code

--- src/solver/cmp.rs
+++ src/solver/cmp.rs
@@ -55,32 +55,3 @@ impl Vars {
         }
     }
 }
-
-impl PartialOrd for Term {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        use std::cmp::Ordering::Equal;
-        use Term::*;
-        match self.ord().partial_cmp(&other.ord()) {
-            Some(Equal) | None => (),
-            other => return other,
-        };
-        match (self, other) {
-            (Nil, Nil) => Some(Equal),
-            (Variable(_, _), Variable(_, _)) => todo!(),
-            (Atom(lhs), Atom(rhs)) => lhs.partial_cmp(rhs),
-            (Struct(lhs_id, lhs_args), Struct(rhs_id, rhs_args)) => {
-                match lhs_args.len().partial_cmp(&rhs_args.len()) {
-                    Some(Equal) | None => (),
-                    other => return other,
-                };
-                match lhs_id.partial_cmp(rhs_id) {
-                    Some(Equal) | None => (),
-                    other => return other,
-                };
-                // this shou,kd search for first non equal
-                zip(lhs_args, rhs_args).find_map(|(a, b)| a.partial_cmp(b))
-            }
-            _ => unreachable!(),
-        }
-    }
-}
